bed0ccdadd5d77722d89ae5ceae334eb33e6a25f
David Szymanski <dszymanski1408@bths.edu>
2017-01-12 20:08:18 -0500

    Update Progress.java

--- SimonJason/src/simonJason/Progress.java
+++ SimonJason/src/simonJason/Progress.java
@@ -1,38 +1,73 @@
 package simonJason;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.text.AttributedCharacterIterator.Attribute;
+import java.util.Map;
 
-import guiCompononets.Component;
+import gui.components.Components;
 
-public class Progress extends Component implements ProgressInterfaceJasonLau {
-
-	public Progress(int x, int y, int w, int h) {
-		super(x, y, w, h);
+public class Progress extends Components implements ProgressInterfaceJasonLau {
+	
+	private boolean gameOver;
+	private static int height = 50;
+	private static int width = 140;
+	private String round;
+	private String sequenceSize;
+	private String font = "Comic Sans MS";
+	private int size = 20;
+	
+	
+	
+	public Progress() {
+		super(60, 60, width, height);
 		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void gameOver() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void setSequenceSize(int size) {
 		// TODO Auto-generated method stub
-
+		sequenceSize = "Length " + size;
+		update();
 	}
 
 	@Override
-	public void setRound(int round) {
+	public void setRound(int roundNumber) {
 		// TODO Auto-generated method stub
-
+		round = "Round #" + roundNumber;
+		update();
 	}
 
 	@Override
-	public void update(Graphics2D arg0) {
+	public void gameOver() {
 		// TODO Auto-generated method stub
+		gameOver = true;
+		update();
+	}
 
+	@Override
+	public void update(Graphics2D g) {
+		// TODO Auto-generated method stub
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.setFont(new Font(font , Font.PLAIN,size));
+		FontMetrics fm = g.getFontMetrics(); 
+		if(!gameOver){
+			g.setColor(Color.green);
+			g.fillRect(0,0,width,height);
+			g.setColor(Color.black);
+			g.drawRect(0,0,width-1, height -1);
+			if(round != null) g.drawString(round, (width - fm.stringWidth(round))/2, 20);
+			if(sequenceSize != null) g.drawString(sequenceSize, (width - fm.stringWidth(sequenceSize))/2, 40);
+		}
+		else{
+			g.setColor(Color.red);
+			g.fillRect(0, 0, width, height);
+			g.setColor(Color.black);
+			g.drawString("Game Over!", (width - fm.stringWidth("Game Over!"))/2, 30);
+		}
 	}
 
 }
