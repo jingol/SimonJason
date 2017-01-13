96c67cfc1229e9df01b9954ba1ee80a7f2896eb5
Jason <jlau6606@bths.edu>
2017-01-12 19:12:38 -0500

    ad

---
+++ SimonJason/src/simonJason/Button.java
@@ -0,0 +1,70 @@
+package simonJason;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import guiCompononets.Action;
+import guiCompononets.Component;
+
+public class Button extends Component implements ButtonInterfaceJasonLau {
+
+	public Button(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isHovered(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setAction(Action a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dim() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setX(int x) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setY(int y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Graphics2D arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SimonJason/src/simonJason/Move.java
@@ -0,0 +1,11 @@
+package simonJason;
+
+public class Move implements MoveInterfaceJasonLau {
+
+	@Override
+	public ButtonInterfaceJasonLau getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
